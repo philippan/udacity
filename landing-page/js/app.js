75708e6f4dd662502a0e375120339be81e0b6ed4
Philip <philipspan@gmail.com>
2020-01-31 15:39:22 -0800

    Added eslint and npm

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -11,72 +11,47 @@
  *
  * JS Standard: ESlint
  *
-*/
-
-
-
+ */
 /**
  * Define Global Variables
  *
-*/
-
+ */
 const main = document.querySelector("main");
 const pageHeader = document.querySelector("header.page__header");
 const navList = document.querySelector("#navbar__list");
 const addButton = document.querySelector("button#addSect");
 const removeButton = document.querySelector("button#removeSect");
 const rewindButton = document.querySelector("button#rewind");
-
-
 /**
  * End Global Variables
  * Start Helper Functions
  *
-*/
-
-
+ */
 /* inView() checks if a <sect> is in the viewport */
-
-function inView () {
-
+function inView() {
     let checker = [];
     let sects = document.querySelectorAll("section");
-
     for (let sect of sects) {
         let objectTop = sect.getBoundingClientRect().top;
-        let objectBottom = sect.getBoundingClientRect().bottom-60;
+        let objectBottom = sect.getBoundingClientRect().bottom - 60;
         let viewportHeight = window.innerHeight;
-
         if ((objectTop > 0 && objectTop < viewportHeight) || (objectBottom > 0 && objectBottom < viewportHeight)) {
-
             checker.push(true);
-
-        }
-
-        else {
-
+        } else {
             checker.push(false);
-
         }
     }
-
     return checker;
-
 }
-
 /**
  * End Helper Functions
  * Begin Main Functions
  *
-*/
-
+ */
 // Build the nav
-
-function navBuild () {
-
+function navBuild() {
     navList.innerHTML = "";
     let sects = document.querySelectorAll("section");
-
     for (let sect of sects) {
         let sectName = sect.getAttribute("data-nav");
         let sectID = sect.getAttribute("id");
@@ -84,80 +59,48 @@ function navBuild () {
         listNode.innerHTML = `<a class="menu__link" href="#${sectID}">${sectName}</a>`;
         navList.appendChild(listNode);
     }
-
     scrollBuild();
-
 }
-
 // Add scrollTo functionality to nav
-
-function scrollBuild () {
-
+function scrollBuild() {
     let navLinks = document.querySelectorAll("a.menu__link");
-
     for (let navLink of navLinks) {
-
         let destinationID = "section" + navLink.getAttribute("href") + " div.landing__container";
         let destinationEl = document.querySelector(destinationID);
-
-        navLink.addEventListener("click", function(event){
-
+        navLink.addEventListener("click", function(event) {
             event.preventDefault();
             destinationEl.scrollIntoView({
                 block: "start",
                 behavior: "smooth"
-
             });
-
         });
-
     }
-
 }
-
-
 // Decide which <sect> is the active one, esp. if multiple are in the viewport
-
-function isActive () {
-
+function isActive() {
     let checker = inView();
     let sects = document.querySelectorAll("section");
-
     /* console.log(checker + " " + sects[0]); */
-
     for (let i = 0; i < sects.length; i++) {
-
         if (checker[0] == true) {
-              for (let sect of sects) {
+            for (let sect of sects) {
+                sect.classList.toggle("your-active-class", false);
+            }
+            sects[0].classList.toggle("your-active-class", true);
+        } else {
+            if (checker[i] == true && checker[i - 1] == false) {
+                for (let sect of sects) {
                     sect.classList.toggle("your-active-class", false);
-              }
-              sects[0].classList.toggle("your-active-class", true);
+                }
+                sects[i].classList.toggle("your-active-class", true);
+            }
         }
-
-        else {
-
-              if (checker[i] == true && checker[i-1] == false) {
-
-                    for (let sect of sects) {
-                          sect.classList.toggle("your-active-class", false);
-                    }
-
-                    sects[i].classList.toggle("your-active-class", true);
-              }
-        }
-
     }
-
 }
-
-
 // Add section
-
-function addSect () {
-
+function addSect() {
     let numSects = document.querySelectorAll("section").length;
     numSects++;
-
     let newSect = `
         <section id="section${numSects}" data-nav="Section ${numSects}">
           <div class="landing__container">
@@ -166,104 +109,59 @@ function addSect () {
           </div>
         </section>
     `;
-
     main.insertAdjacentHTML('beforeend', newSect);
     navBuild();
-
 }
-
 // Remove section
-
-function removeSect () {
-
+function removeSect() {
     let numSects = document.querySelectorAll("section").length;
     let targetSect = document.querySelector(`section#section${numSects}`);
     main.removeChild(targetSect);
     navBuild();
-
 }
-
-
 // Show rewind button if below fold
-
-function rewindCheck () {
-
-    let scrollRatio = window.scrollY/document.body.clientHeight;
-
+function rewindCheck() {
+    let scrollRatio = window.scrollY / document.body.clientHeight;
     if (scrollRatio > 0.4) {
         rewindButton.classList.add("buttonVisible");
-    }
-    else {
+    } else {
         rewindButton.classList.remove("buttonVisible");
     }
-
 }
-
 // Rewind to top on clicks
-
-function rewind () {
-
+function rewind() {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-
 }
-
-
 // Hide page header on scroll like mobile browsers
-
 function pageHeaderCheck() {
-
     let isScrolling;
-
-    window.addEventListener("scroll", function(){
-
+    window.addEventListener("scroll", function() {
         window.clearTimeout(isScrolling);
-        /* console.log( 'Scrolling...' ); */
-
         pageHeader.classList.add("page__header__whileScroll");
-
         isScrolling = setTimeout(function() {
-
             pageHeader.classList.remove("page__header__whileScroll");
-		        /* console.log( 'Scrolling has stopped.' ); */
-
-	       }, 300);
-
-
+        }, 300);
     }, false);
-
 }
-
-
 /**
  * End Main Functions
  * Begin Events
  *
-*/
-
+ */
 // DOM loaded
-
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener('DOMContentLoaded', function() {
     navBuild();
-
 });
-
-
 // On scroll
-
-document.addEventListener("scroll", function () {
-
+document.addEventListener("scroll", function() {
     isActive();
     rewindCheck();
     pageHeaderCheck();
-
 });
-
 // On click
-
 addButton.addEventListener("click", addSect);
 removeButton.addEventListener("click", removeSect);
 rewindButton.addEventListener("click", rewind);
